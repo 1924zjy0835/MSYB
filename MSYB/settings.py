09f26aa5e3582f6d84b8650a20363dfd99637a6a
1924zjy0835 <1924083517@qq.com>
2021-05-07 16:10:23 +0800

    更改静态文件路径

--- MSYB/settings.py
+++ MSYB/settings.py
@@ -153,10 +153,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front', 'dist')
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'front', 'dist')
+# ]
 
 # 配置一个静态文件的存储路径，以便后期ngix去寻找
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')
