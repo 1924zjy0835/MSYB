540220cb9513c95d113ea28f43f7d76ea60854bd
1924zjy0835 <1924083517@qq.com>
2021-05-08 19:23:00 +0800

    订单页完成，并且将clothorder表中的uid更改为了id

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -33,7 +33,7 @@ class Clothes(models.Model):
 
 
 class ClothesOrder(models.Model):
-    uid = ShortUUIDField(primary_key=True)
+    # uid = ShortUUIDField(primary_key=True)
     cloth = models.ForeignKey("Clothes", on_delete=models.DO_NOTHING)
     buyer = models.ForeignKey("msybauth.User", on_delete=models.DO_NOTHING)
     amount = models.FloatField(default=0)
@@ -42,3 +42,6 @@ class ClothesOrder(models.Model):
     istype = models.SmallIntegerField(default=0)
     # 1： 代表的是未支付；2：代表的是已经支付成功；
     status = models.SmallIntegerField(default=1)
+
+    class Meta:
+        db_table = "clothesorder"
